5630683dbdc0b66f13008661f27f98d4a5ea3042
Alex Selby <david.alex.selby@gmail.com>
2018-09-23 21:06:06 -0700

    Video 5

--- Week1.r
+++ Week1.r
@@ -65,3 +65,40 @@ rm(WHO_Europe)
 
 # Double check it has been removed
 ls()
+
+# Video 5
+# Access a specific variable in a data frame using $
+WHO$Under15
+
+# Compute mean of this variable
+mean(WHO$Under15)
+
+# Compute standard deviation of this variable
+sd(WHO$Under15)
+
+# Get summary for this variable
+summary(WHO$Under15)
+
+# See what country is the min
+which.min(WHO$Under15)
+
+# To see which country is in that row
+WHO$Country[86]
+
+# Now see country with max
+which.max(WHO$Under15)
+
+# To see which country is in that row
+WHO$Country[124]
+
+# Create a scatterplot of GNI versus Fertility Rate
+plot(WHO$GNI, WHO$FertilityRate)
+
+# See which countries are outliers
+outliers = subset(WHO, GNI > 10000 & FertilityRate > 2.5)
+
+# See how many rows of data are in our subset
+nrow(outliers)
+
+# See country names in subset by making a vector of the names of the variables
+outliers[c("Country","GNI","FertilityRate")]
