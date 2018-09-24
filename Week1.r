5f8f4ecf63e47093c01b74006f425d403fa950bd
Alex Selby <david.alex.selby@gmail.com>
2018-09-23 20:50:13 -0700

    Video 4

--- Week1.r
+++ Week1.r
@@ -32,3 +32,36 @@ NewCountryData
 
 AllCountryData = rbind(CountryData, NewCountryData)
 AllCountryData
+
+# Change Working Directory
+setwd("~/Desktop/AnalyticsEdge")
+
+# Check Working Directory
+getwd()
+
+# Read In File and Save to Dataframe "who"
+WHO = read.csv("WHO.csv")
+
+# Look at data
+str(WHO)
+
+# Get a summary
+summary(WHO)
+
+# Create a new dataframe with only countries in EU
+WHO_Europe = subset(WHO, Region == "Europe")
+
+# Explore the new dataframe
+str(WHO_Europe)
+
+# Export to a CSV
+write.csv(WHO_Europe, "Who_Europe.csv")
+
+# See what variables we are working with
+ls()
+
+# Remove is rm
+rm(WHO_Europe)
+
+# Double check it has been removed
+ls()
